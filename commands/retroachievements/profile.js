3582414e072bfd78677220c44aab59afbb29d3b7
charlin99 <cmiguelsantosfilho@gmail.com>
2024-10-21 23:38:20 -0300

    Ajustando exibição do embed do perfil

--- commands/retroachievements/profile.js
+++ commands/retroachievements/profile.js
@@ -20,15 +20,19 @@ module.exports = {
 
         if (userProfile) {
             if (userProfile) {
+                const memberSinceDate = new Date(userProfile.memberSince);
+                const formattedDate = new Intl.DateTimeFormat('pt-BR').format(memberSinceDate);    
                 const profilePicUrl = `https://media.retroachievements.org${userProfile.userPic}`
                 const embed = new EmbedBuilder()
-                    .setTitle(`RetroAchievements de ${userProfile.user}`)
-                    .setDescription(`*${userProfile.motto}*
-                                    \nPontuação: ${userProfile.totalPoints} (${userProfile.totalTruePoints})
-                                    \nMembro desde: ${userProfile.memberSince}`)
-                    .setImage(profilePicUrl) // Aqui exibimos a imagem do perfil
-                    .setColor('#00FF00'); // Cor opcional
-    
+                    .setTitle(`${userProfile.user}`)
+                    .setURL(`https://retroachievements.org/user/${userProfile.user}`)
+                    .setAuthor({ name: 'Perfil do RetroAchievements', iconURL: 'https://static.retroachievements.org/assets/images/ra-icon.webp', url: 'https://retroachievements.org/' })
+                    .setDescription(`*${userProfile.motto}*`)
+                    .setThumbnail(profilePicUrl) // Aqui exibimos a imagem do perfil
+                    .addFields(
+                        { name: `**Pontuação**`, value: `${userProfile.totalPoints} (${userProfile.totalTruePoints})`, inline: true },
+                        { name: `**Membro desde**`, value: `${formattedDate}`, inline: true },
+                    )
                 await interaction.reply({ embeds: [embed] });
             } else {
                 await interaction.reply('Não foi possível obter o perfil.');
