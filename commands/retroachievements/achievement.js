6706a01957343a3fb1712c92dc6e7c6ce91de9c6
cmiguelfilho <cmiguelsantosfilho@gmail.com>
2024-10-22 11:42:28 -0300

    Refatoração de código

--- commands/retroachievements/achievement.js
+++ commands/retroachievements/achievement.js
@@ -1,35 +1,57 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { username, webApiKey } = require('../../config.json');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { username, webApiKey } = require("../../config.json");
 
-const { buildAuthorization, getGameExtended } = require("@retroachievements/api");
+const {
+  buildAuthorization,
+  getGameExtended,
+} = require("@retroachievements/api");
 
 const authorization = buildAuthorization({ username, webApiKey });
 
-module.exports = {
+const data = new SlashCommandBuilder()
+  .setName("achievement")
+  .setDescription("Get RA achievement!");
 
-	data: new SlashCommandBuilder()
-		.setName('achievement')
-		.setDescription('Get RA achievement!'),
-	async execute(interaction) {
-        const gameExtended = await getGameExtended(authorization, {
-            gameId: 11279,
-        });
-        if (gameExtended) {
-            const achievementPic = `https://media.retroachievements.org/Badge/${gameExtended.achievements[284533].badgeName}.png`;
-            const embed = new EmbedBuilder()                    
-                .setTitle(`🏆 ${gameExtended.achievements[284533].title}`)
-                .setURL(`https://retroachievements.org/achievement/284533`)
-                .setAuthor({ name: 'Conquista do RetroAchievements', iconURL: 'https://static.retroachievements.org/assets/images/ra-icon.webp', url: 'https://retroachievements.org/' })
-                .setDescription(`*${gameExtended.achievements[284533].description}*`)
-                .setThumbnail(achievementPic) // Aqui exibimos a imagem da conquista
-                .addFields(
-                    { name: '**Pontos**', value: `${gameExtended.achievements[284533].points} (${gameExtended.achievements[284533].trueRatio})` },
-                    { name: '**Jogo**', value: `${gameExtended.title}` }
-                )
-                .setImage(`https://media.retroachievements.org${gameExtended.imageIcon}`)
-            await interaction.reply({ embeds: [embed] });
-        } else {
-            await interaction.reply('Não foi possível obter a conquista.');
-        }
+async function execute(interaction) {
+  try {
+    const gameExtended = await getGameExtended(authorization, {
+      gameId: 11279,
+    });
+
+    if (!gameExtended) {
+      throw new Error("Failed to retrieve game information");
     }
+
+    const achievementPic = `https://media.retroachievements.org/Badge/${gameExtended.achievements[284533].badgeName}.png`;
+    
+    const embed = new EmbedBuilder()
+      .setTitle(`🏆 ${gameExtended.achievements[284533].title}`)
+      .setURL(`https://retroachievements.org/achievement/284533`)
+      .setAuthor({
+        name: "Conquista do RetroAchievements",
+        iconURL:
+          "https://static.retroachievements.org/assets/images/ra-icon.webp",
+        url: "https://retroachievements.org/",
+      })
+      .setDescription(`*${gameExtended.achievements[284533].description}*`)
+      .setThumbnail(achievementPic)
+      .addFields(
+        {
+          name: "**Pontos**",
+          value: `${gameExtended.achievements[284533].points} (${gameExtended.achievements[284533].trueRatio})`,
+        },
+        { name: "**Jogo**",
+          value: `${gameExtended.title}` }
+      )
+      .setImage(`https://media.retroachievements.org${gameExtended.imageIcon}`);
+
+    await interaction.reply({ embeds: [embed] });
+  } catch (error) {
+    await interaction.reply("Não foi possível obter a conquista.");
+  }
 }
+
+module.exports = {
+  data,
+  execute
+};
--- commands/retroachievements/profile.js
+++ commands/retroachievements/profile.js
@@ -1,48 +1,74 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { username, webApiKey } = require('../../config.json');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { username, webApiKey } = require("../../config.json");
 
-const { buildAuthorization, getUserProfile } = require("@retroachievements/api");
+const { 
+    buildAuthorization,
+    getUserProfile
+} = require("@retroachievements/api");
 
 const authorization = buildAuthorization({ username, webApiKey });
 
-module.exports = {
+const data = new SlashCommandBuilder()
+    .setName("profile")
+    .setDescription("Get RA profile!")
+    .addStringOption(option =>
+        option.setName("nome")
+                .setDescription("Nome do perfil")
+                .setRequired(true));
 
-	data: new SlashCommandBuilder()
-		.setName('profile')
-		.setDescription('Get RA profile!')
-        .addStringOption(option =>
-            option.setName('nome')
-                .setDescription('Nome do perfil')
-                .setRequired(true)),
-	async execute(interaction) {
-        const username = interaction.options.getString('nome');
-        const userProfile = await getUserProfile(authorization, { username });
+async function execute(interaction) {
+    try {
+        const userProfile = await getUserProfile(authorization, {
+            username
+        });
 
-        if (userProfile) {
-            const memberSinceDate = new Date(userProfile.memberSince);
-            memberSinceDate.setHours(memberSinceDate.getHours() - 3);
-            const formattedDate = new Intl.DateTimeFormat('pt-BR', {
-                day: '2-digit',
-                month: '2-digit',
-                year: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-            }).format(memberSinceDate);    
-            const profilePicUrl = `https://media.retroachievements.org${userProfile.userPic}`
-            const embed = new EmbedBuilder()
+        if (!userProfile) {
+            throw new Error("Failed to retrieve user information");
+        }
+
+        const username = interaction.options.getString("nome");
+
+        const profilePicUrl = `https://media.retroachievements.org${userProfile.userPic}`;
+
+        const memberSinceDate = new Date(userProfile.memberSince);
+        memberSinceDate.setHours(memberSinceDate.getHours() - 3); // Fuso horário UTC - 3
+        const formattedDate = new Intl.DateTimeFormat('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+        }).format(memberSinceDate); // Formata pro padrão brasileiro
+
+        const embed = new EmbedBuilder()
                 .setTitle(`${userProfile.user}`)
                 .setURL(`https://retroachievements.org/user/${userProfile.user}`)
-                .setAuthor({ name: 'Perfil do RetroAchievements', iconURL: 'https://static.retroachievements.org/assets/images/ra-icon.webp', url: 'https://retroachievements.org/' })
+                .setAuthor({
+                    name: "Perfil do RetroAchievements",
+                    iconURL: "https://static.retroachievements.org/assets/images/ra-icon.webp",
+                    url: "https://retroachievements.org/"
+                })
                 .setDescription(`*${userProfile.motto}*`)
                 .setThumbnail(profilePicUrl) // Aqui exibimos a imagem do perfil
-                .addFields(
-                    { name: `**Pontuação**`, value: `${userProfile.totalPoints} (${userProfile.totalTruePoints})`, inline: true },
-                    { name: `**Membro desde**`, value: `${formattedDate}`, inline: true },
-                )
-            await interaction.reply({ embeds: [embed] });
-        } else {
-            await interaction.reply('Não foi possível obter o perfil.');
-        }
+                .addFields({
+                        name: "**Pontuação**",
+                        value: `${userProfile.totalPoints} (${userProfile.totalTruePoints})`,
+                        inline: true
+                    },
+                    {
+                        name: "**Membro desde**",
+                        value: `${formattedDate}`,
+                        inline: true
+                    })
+
+        await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+        await interaction.reply("Não foi possível obter o perfil.");
     }
 }
+
+module.exports = {
+    data,
+    execute
+};
