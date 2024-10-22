7a3dc91cd324d71cfc26b0acef818f8bc0297ee4
cmiguelfilho <cmiguelsantosfilho@gmail.com>
2024-10-22 10:52:15 -0300

    Removendo if desnecessário em profile.js

--- commands/retroachievements/profile.js
+++ commands/retroachievements/profile.js
@@ -19,32 +19,30 @@ module.exports = {
         const userProfile = await getUserProfile(authorization, { username });
 
         if (userProfile) {
-            if (userProfile) {
-                const memberSinceDate = new Date(userProfile.memberSince);
-                memberSinceDate.setHours(memberSinceDate.getHours() - 3);
-                const formattedDate = new Intl.DateTimeFormat('pt-BR', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    second: '2-digit',
-                }).format(memberSinceDate);    
-                const profilePicUrl = `https://media.retroachievements.org${userProfile.userPic}`
-                const embed = new EmbedBuilder()
-                    .setTitle(`${userProfile.user}`)
-                    .setURL(`https://retroachievements.org/user/${userProfile.user}`)
-                    .setAuthor({ name: 'Perfil do RetroAchievements', iconURL: 'https://static.retroachievements.org/assets/images/ra-icon.webp', url: 'https://retroachievements.org/' })
-                    .setDescription(`*${userProfile.motto}*`)
-                    .setThumbnail(profilePicUrl) // Aqui exibimos a imagem do perfil
-                    .addFields(
-                        { name: `**Pontuação**`, value: `${userProfile.totalPoints} (${userProfile.totalTruePoints})`, inline: true },
-                        { name: `**Membro desde**`, value: `${formattedDate}`, inline: true },
-                    )
-                await interaction.reply({ embeds: [embed] });
-            } else {
-                await interaction.reply('Não foi possível obter o perfil.');
-            }
+            const memberSinceDate = new Date(userProfile.memberSince);
+            memberSinceDate.setHours(memberSinceDate.getHours() - 3);
+            const formattedDate = new Intl.DateTimeFormat('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+            }).format(memberSinceDate);    
+            const profilePicUrl = `https://media.retroachievements.org${userProfile.userPic}`
+            const embed = new EmbedBuilder()
+                .setTitle(`${userProfile.user}`)
+                .setURL(`https://retroachievements.org/user/${userProfile.user}`)
+                .setAuthor({ name: 'Perfil do RetroAchievements', iconURL: 'https://static.retroachievements.org/assets/images/ra-icon.webp', url: 'https://retroachievements.org/' })
+                .setDescription(`*${userProfile.motto}*`)
+                .setThumbnail(profilePicUrl) // Aqui exibimos a imagem do perfil
+                .addFields(
+                    { name: `**Pontuação**`, value: `${userProfile.totalPoints} (${userProfile.totalTruePoints})`, inline: true },
+                    { name: `**Membro desde**`, value: `${formattedDate}`, inline: true },
+                )
+            await interaction.reply({ embeds: [embed] });
+        } else {
+            await interaction.reply('Não foi possível obter o perfil.');
         }
     }
 }
